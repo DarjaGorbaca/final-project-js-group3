b0259fa9dd2b1bd6f910e3c778e199f749d29865
Your Name <you@example.com>
2022-06-19 15:34:51 +0300

    update6

--- scripts.js
+++ scripts.js
@@ -1,16 +1,22 @@
+// dark/light mode
+
 const switchElement = document.querySelector('.switch')
 
 switchElement.addEventListener('click', () => {
     document.body.classList.toggle('dark')
 })
 
+// gallery
+
 const imageContainer = document.getElementById('content-container');
 
 let photosArray = [];
 
-const count = 1;
+const count = 3;
+let query = 'mountains';
+let orientation = 'portrait';
 const apiKey = '0pJsGzy6XnFXrnT9JV75k98ppLBETQJJG0uQuzTkTug';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&query=${query}&orientation=${orientation}`;
 
 function displayPhotos() {
 photosArray.forEach((photo) => {
@@ -39,3 +45,20 @@ async function getPhotos() {
 }
 getPhotos();
 
+// slider
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
